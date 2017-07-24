434b7f8efb53c4a3c557f3210508046737b598b7
雷金磊 <leijinlei_phper@163.com>
2017-07-24 13:50:20 +0800

    引入laravel-admin模块
    引入laravel-admin模块

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // 引入laravel-admin
+        Encore\Admin\Providers\AdminServiceProvider::class,
+        
     ],
 
     /*
